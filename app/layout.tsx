5485b4921fe2741cd63c5ac9a3036c7d6325be5e
Dongmin Jang (장동민) <dogzz9445@gmail.com>
2024-07-12 15:30:43 +0900

    Remove banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
           >
             <CartProvider>
               <div>
-                <Banner />
+                {/* <Banner /> */}
                 <Header />
                 {children}
               </div>
